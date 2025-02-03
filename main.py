b00194d293eebdcbe5473e161a1b6080a40837f7
Mickilenzy <ilerioluwamichael297@gmail.com>
2025-02-03 11:29:14 -0600

    Add files via upload

---
+++ main.py
@@ -0,0 +1,25 @@
+name = "Michael"
+age  = 25 
+is_student = True 
+weight = 47.5
+
+print("Name :", name)
+print("Data Type of Name is", type(name))
+
+print("Age : ", age)
+print("Date Type of Age is", type(age))
+
+
+print("is_student ", is_student )
+print("Data Type of is_student is  ",type( is_student ))
+
+print("Weight :", weight)
+print("Data Type of weight is  ", type(weight))
+
+print("\n After Type Casting.....")
+age = str(age)
+print(age)
+print("Data Type of age is ", type(age))
+weight = int(weight )
+print(weight )
+print("Data Type of weight is", type(weight) )
